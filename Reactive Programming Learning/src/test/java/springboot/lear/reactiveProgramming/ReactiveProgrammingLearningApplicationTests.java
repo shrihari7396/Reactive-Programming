68500e5e4a4ad600a6d251bac67cbe7c787493c3
SHRIHARI KULKARNI <shriharik121@gmail.com>
2025-09-19 12:38:18 +0530

    First Commit

--- Reactive Programming Learning/src/test/java/springboot/lear/reactiveProgramming/ReactiveProgrammingLearningApplicationTests.java
+++ Reactive Programming Learning/src/test/java/springboot/lear/reactiveProgramming/ReactiveProgrammingLearningApplicationTests.java
@@ -1,13 +1,41 @@
 package springboot.lear.reactiveProgramming;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 class ReactiveProgrammingLearningApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void firstTest() {
+		Mono<String> mono = Mono.just("Testing");
+
+		mono.subscribe(new CoreSubscriber<String>() {
+			@Override
+			public void onSubscribe(Subscription s) {
+				System.out.println("Subscribed Successfully !!!");
+				s.request(1);
+			}
+			@Override
+			public void onNext(String data) {
+				System.out.println("data :-" + data);
+			}
+			@Override
+			public void onError(Throwable t) {
+				System.out.println("error :-" + t.getMessage());
+			}
+			@Override
+			public void onComplete() {
+				System.out.println("Completed Successfully !!!");
+			}
+		});
 	}
 
 }
